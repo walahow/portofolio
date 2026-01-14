ffbaff82e6071e65d8e16f5d7a98746fa181ab5d
walahow <latifhamzah106@gmail.com>
2026-01-14 14:02:24 +0700

    ytmaodwc

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -31,8 +31,7 @@ export default function SmoothScroll({
         // }
 
         const lenisInstance = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            lerp: 0.1, // Smoother damping for "soft stop" effect
             orientation: "vertical",
             gestureOrientation: "vertical",
             smoothWheel: true,
